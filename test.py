e8093b7106953245d2bffe68551a327e5434aaf2
iKiway <105075696+iKiway@users.noreply.github.com>
2025-05-19 15:11:31 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,6 @@
-train = []
-print(len(train[0:2]))
+from db_api.Station import Station
+import datetime
+
+
+station = Station("Rommelshausen","529fc99d86062cff082818f1820c4900","ef252166427b5094f093b9e5f331508c")
+station.get_sorted_departure_list()
