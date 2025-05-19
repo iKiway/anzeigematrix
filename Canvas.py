3f5bfd48f1f589265b369e131f3f45b6cb3ad2a3
iKiway <105075696+iKiway@users.noreply.github.com>
2025-05-19 20:08:40 +0200

    Update Canvas.py

--- Canvas.py
+++ Canvas.py
@@ -117,7 +117,7 @@ class MatrixHelper:
                 self.matrix.SwapOnVSync(self.canvas)
                 self.display_final_destination_new(destination_up, pos_up, graphics.Color(0,0,0), upper=True)
                 if len(train_list) > 1:
-                    self.display_final_destination_new(destination_down, pos_down, graphics.Color(0,0,0), upper=False)
+                    self.display_final_destination_new(destination_down, pos_up, graphics.Color(0,0,0), upper=False)
                 if text_width_up > 43:
                     pos_up -= 1
                 if text_width_down > 43:
