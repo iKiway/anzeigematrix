e4fe60e1dbee8858a291423076fac4186ba62f0e
iKiway <105075696+iKiway@users.noreply.github.com>
2025-07-10 20:08:01 +0200

    alte löschen

--- Clock.py
+++ Clock.py
@@ -9,6 +9,7 @@ class Clock(App):
         # Hier kannst du den Hintergrund für die Uhr setzen
         self.canvas.Clear()
         self.canvas.SetPixel(0, 0, self.graphics_accent_color.red, self.graphics_accent_color.green, self.graphics_accent_color.blue)
+        self.matrix.SwapOnVSync(self.canvas)
         
     def start_display(self):
         self.background()
